d90a99b71c3d8730b710d484d88c4a8942afef4c
Jose Valenzuela <valenz74@gmail.com>
2022-05-19 18:37:16 -0500

    Aprendiendo sobre Operadores Aritmeticos en Java

---
+++ src/OperadoresAritmeticos.java
@@ -0,0 +1,18 @@
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        //OPERADORES ARITMETICOS
+
+    /*
+        + - * /
+        Jerarquía:
+            Operador . (POO), Operador ()
+            Primero : * / (con prioridad de izquierda a derecha)
+            Segunda : + - (con prioridad de izquierda a derecha)
+
+     */
+        int a = (3 + 3) * 2 - 3;
+        System.out.println(a);
+    }
+
+}
